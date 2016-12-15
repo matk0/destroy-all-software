ee6d69442ae1d41c91a6b6a4fe39822b43dbbf03
matk0 <matej@occupationbliss.com>
2016-12-15 07:43:48 +0100

    Work out 003 Building RSpec From Scratch.

---
+++ 003 Building Rspec From Scratch/test_spec.rb
@@ -0,0 +1,34 @@
+require 'test/unit'
+require './spec'
+
+class TestDescribe < Test::Unit::TestCase
+  def test_that_it_can_pass
+    describe 'some thing' do
+      it 'has some property' do
+      end
+    end
+  end
+
+  def test_that_it_can_fail
+    assert_raise(IndexError) do
+      describe 'some failing thing' do
+        it 'fails' do
+          raise IndexError
+        end
+      end
+    end
+  end
+end
+
+
+class TestAssertion < Test::Unit::TestCase
+  def test_that_it_can_pass
+    2.should == 2
+  end
+
+  def test_that_it_can_fail
+    assert_raise(AssertionError) do
+      1.should == 2
+    end
+  end
+end
