9a99b87d104be40b1b5116d19259fcee039b97ab
matk0 <matej@occupationbliss.com>
2016-12-30 13:20:01 +0100

    Add properly designed tests(V2) for getting energy from food.

--- 007 Growing A Test Suite/walrus.rb
+++ 007 Growing A Test Suite/walrus.rb
@@ -1,22 +1,40 @@
 class Walrus
+  attr_reader :energy
+
+  def initialize
+    @energy = 0
+  end
+
   def receive_gift! gift
     if gift.edible?
-      gift.digest!
+      @energy += gift.digest.energy
     end
   end
 end
 
+class LifeEvent
+  attr_reader :energy
+
+  def initialize params
+    @energy = params.fetch(:energy)
+  end
+end
+
 describe Walrus do
-  it "eats food" do
+  it "gains energy by eating food" do
     cheese = double("cheese")
-    allow(cheese).to receive(:edible?) {true}
-    cheese.should_receive(:digest!)
-    subject.receive_gift!(cheese)
+    allow(cheese).to receive_messages(:edible? => true, :digest => LifeEvent.new(:energy => 100))
+
+    expect do
+      subject.receive_gift!(cheese)
+    end.to change { subject.energy }.by 100
   end
 
   it "ignores non-edible things" do
     shoes = double("shoes")
     allow(shoes).to receive(:edible?) {false}
-    subject.receive_gift!(shoes)
+    expect do
+      subject.receive_gift!(shoes)
+    end.not_to change { subject.energy }
   end
 end
